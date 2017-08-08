6e0c1241e6ded78e70f0360f30ccf9c003087118
Jan Marker <jan.marker@kdab.com>
2017-08-08 10:42:18 +0800

    Also install new grid_view

--- kdab_install.sh
+++ kdab_install.sh
@@ -13,7 +13,7 @@ pip install --prefix=~/opt -e pkg || exit 1
 pip install --prefix=~/opt mock || exit 1
 
 # copied from Buildbot's Makefile, adapt as required
-for i in base wsgi_dashboards codeparameter console_view waterfall_view nestedexample; do
+for i in base wsgi_dashboards codeparameter console_view waterfall_view grid_view nestedexample; do
   cd www/${i}
   py_install
   cd ../..
