5d46379d2bbbbf925276254f2e1f730fee3a9b28
Jan Marker <jan.marker@kdab.com>
2017-09-11 17:25:59 +0200

    Also build data_module on our own

--- kdab_install.sh
+++ kdab_install.sh
@@ -12,6 +12,11 @@ cd ../
 pip install --prefix=~/opt -e pkg || exit 1
 pip install --prefix=~/opt mock || exit 1
 
+pushd www/data_module
+npm install
+node_modules/.bin/gulp prod --no-tests
+popd
+
 # copied from Buildbot's Makefile, adapt as required
 for i in base wsgi_dashboards codeparameter console_view waterfall_view grid_view nestedexample; do
   cd www/${i}
