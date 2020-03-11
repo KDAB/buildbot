7cf8c899b3bba25125068e95104abd2ba5fa09ca
Buildmaster <buildmaster@kdab.com>
2020-03-11 15:53:58 +0100

    Wild hacks to make build work

--- kdab_install.sh
+++ kdab_install.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 # required for yarn
-export PATH="$PATH:$PWD/node_modules/.bin"
+export PATH="$PATH:$PWD/node_modules/.bin/"
+npm install yarn
 
 PREFIX=~/opt/buildbot
 
@@ -20,16 +21,30 @@ cd ../
 pip3 install --system --prefix=$PREFIX -e pkg || exit 1
 pip3 install --system --prefix=$PREFIX mock || exit 1
 
+
+pushd www/build_common
+yarn install
+popd
+
+pushd www/
+cp -r build_common/node_modules .
+popd
+
+pushd www/guanlecoja-ui/
+yarn install
+npm install acorn-walk
+yarn build
+popd
+
 pushd www/data_module
 yarn install
-node_modules/.bin/gulp prod --no-tests
 popd
 
 # copied from Buildbot's Makefile, adapt as required
 # DISABLED: console_view waterfall_view grid_view
 for i in base wsgi_dashboards codeparameter nestedexample; do
   pushd www/${i}
-  yarn install
+  export PATH="$PATH:$PWD/node_modules/.bin/"
   py_install
   popd
 done
