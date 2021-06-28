85d241821b9f983ff72feca6291b021ff3d79c28
Povilas Kanapickas <povilas@radix.lt>
2021-06-28 22:47:54 +0300

    Implement service to cancel builds after their branches get new commits

--- master/setup.py
+++ master/setup.py
@@ -388,6 +388,7 @@ setup_args = {
                 'CommandlineUserManager']),
             ('buildbot.revlinks', ['RevlinkMatch']),
             ('buildbot.reporters.utils', ['URLForBuild']),
+            ('buildbot.schedulers.canceller', ['OldBuildCanceller']),
             ('buildbot.schedulers.forcesched', [
                 'AnyPropertyParameter', 'BooleanParameter',
                 'ChoiceStringParameter',
