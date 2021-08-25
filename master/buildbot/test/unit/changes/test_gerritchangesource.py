0ea7c6a12742f07678159cfe08b98dab57f350c1
Povilas Kanapickas <povilas@radix.lt>
2021-08-26 02:06:19 +0300

    changes: Ignore gerrit ref-updated events if they are for change branch

--- master/buildbot/test/unit/changes/test_gerritchangesource.py
+++ master/buildbot/test/unit/changes/test_gerritchangesource.py
@@ -371,6 +371,26 @@ class TestGerritChangeSource(changesource.ChangeSourceMixin,
         })
 
     @defer.inlineCallbacks
+    def test_lineReceived_ref_updated_for_change(self):
+        s = self.newChangeSource('somehost', 'someuser')
+        yield s.lineReceived(json.dumps({
+            'type': 'ref-updated',
+            'submitter': {
+                'name': 'tester',
+                'email': 'tester@example.com',
+                'username': 'tester'
+            },
+            'refUpdate': {
+                'oldRev': '00000000',
+                'newRev': '56785678',
+                'refName': 'refs/changes/12/432112/1',
+                'project': 'test'
+            },
+            'eventCreatedOn': 1614528683
+        }))
+        self.assertEqual(len(self.master.data.updates.changesAdded), 0)
+
+    @defer.inlineCallbacks
     def test_duplicate_events_ignored(self):
         s = self.newChangeSource('somehost', 'someuser')
         yield s.lineReceived(json.dumps(self.patchset_created_event))
